478de2f42fae392e385ffe7050a7154153fda007
NathanaelZondo <fumanizondo@gmail.com>
2021-07-07 15:38:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sfmyo8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sfmyo8)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>I am coding</h1>
+      <p>I am happy</p>
+    </div>
+  );
+}
